0d5755fb09cf2330e6c1289587ecce54c9c8fe01
Jonathan Lancaster <jcast90@me.com>
2017-11-26 10:16:30 -0800

    messing with reducers/actions

--- client/src/js/components/Login/Login.jsx
+++ client/src/js/components/Login/Login.jsx
@@ -1,10 +1,215 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
+import { postCustomer } from './loginActions';
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      signUp: true,
+      username: 'jcast90',
+      userType: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: 'test',
+      address1: '',
+      address2: '',
+      city: '',
+      state: ''
+    };
+    this.renderLogin = this.renderLogin.bind(this);
+    this.handleLoginState = this.handleLoginState.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleLoginState() {
+    this.state.signUp
+      ? this.setState({
+          signUp: false
+        })
+      : this.setState({
+          signUp: true
+        });
+  }
+  handleChange(key) {
+    return e => this.setState({ [key]: e.target.value });
+  }
+  handleSubmit() {
+    const customerInfo = { ...this.state };
+    this.props.dispatch(postCustomer(customerInfo));
+  }
+  renderLogin() {
+    if (this.state.signUp) {
+      return (
+        <form>
+          <h3>Sign Up</h3>
+          <div className="form-group">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              className="form-control"
+              id="firstName"
+              type="text"
+              value={this.state.firstName}
+              onChange={this.handleChange('firstName')}
+            />
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              className="form-control"
+              id="lastName"
+              type="text"
+              value={this.state.lastName}
+              onChange={this.handleChange('lastName')}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputEmail" className="label name">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              aria-describedby="emailHelp"
+              value={this.state.email}
+              onChange={this.handleChange('email')}
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputPassword">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              value={this.state.password}
+              onChange={this.handleChange('password')}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              id="address1"
+              value={this.state.address1}
+              onChange={this.handleChange('address1')}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="city">City</label>
+            <input
+              type="text"
+              className="form-control"
+              id="city"
+              value={this.state.city}
+              onChange={this.handleChange('city')}
+            />
+            <label htmlFor="state">State</label>
+            <input
+              type="text"
+              className="form-control"
+              id="state"
+              value={this.state.state}
+              onChange={this.handleChange('state')}
+            />
+            <label htmlFor="zip">Zip</label>
+            <input
+              type="text"
+              className="form-control"
+              id="zip"
+              value={this.state.zip}
+              onChange={this.handleChange('zip')}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleSelect1">Register as</label>
+            <select
+              className="form-control"
+              id="exampleSelect1"
+              value={this.state.userType}
+              onChange={this.handleChange('userType')}
+            >
+              <option>Restaurant Owner</option>
+              <option>Customer</option>
+            </select>
+          </div>
+          <button
+            className="submit-btn btn btn-primary"
+            type="submit"
+            onClick={this.handleSubmit}
+          >
+            Sign Up!
+          </button>
+          <button
+            className="login btn btn-secondary"
+            onClick={this.handleLoginState}
+          >
+            Already have an account? Login
+          </button>
+        </form>
+      );
+    } else {
+      return (
+        <div>
+          <h3>Login</h3>
+          <div className="form-group">
+            <label htmlFor="inputEmail" className="label name">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              aria-describedby="emailHelp"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputPassword">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              aria-describedby="passwordHelp"
+            />
+
+            <small id="passwordHelp" className="form-text text-muted">
+              <div className="form-check">
+                <label className="form-check-label">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    onChange={this.forgotPassword}
+                  />
+                  Forgot your password?
+                </label>
+              </div>
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleSelect1">Sign in as</label>
+            <select className="form-control" id="exampleSelect1">
+              <option>Restaurant Owner</option>
+              <option>Customer</option>
+            </select>
+          </div>
+          <button className="submit-btn btn btn-primary" type="submit">
+            Login
+          </button>
+
+          <button className="btn btn-secondary" onClick={this.handleLoginState}>
+            Need to Sign Up?
+          </button>
+        </div>
+      );
+    }
   }
   render() {
-    return <div />;
+    return <div className="jumbotron login-wrapper">{this.renderLogin()}</div>;
   }
 }
--- client/src/js/components/Login/loginActions.js
+++ client/src/js/components/Login/loginActions.js
@@ -1,1 +1,29 @@
-export const types = {};
+import axios from 'axios';
+
+export const types = {
+  POST_CUSTOMER: 'POST_CUSTOMER',
+  POST_RESTAURANT: 'POST_RESTAURANT'
+};
+export function postCustomer(customerInfo) {
+  const { username, email, password } = customerInfo;
+  console.log({ username, email, password });
+  return {
+    type: types.POST_CUSTOMER,
+    payload: axios
+      .post(
+        'https://grubtoeat.herokuapp.com/api/Customers',
+        JSON.stringify({
+          username,
+          email,
+          password
+        })
+      )
+      .then(res => {
+        console.log(res.data);
+        return {
+          user: res.data
+        };
+      })
+      .catch(err => console.log(err))
+  };
+}
