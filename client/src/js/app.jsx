a4e2824b3fdaa5d1d713cfaf5351ec6d56c63f09
Chris Bremmer <c_bremmer@outlook.com>
2017-11-28 18:32:03 -0800

    Up to date restaurantProfile component

--- client/src/js/app.jsx
+++ client/src/js/app.jsx
@@ -1,21 +1,12 @@
-import React from 'react';
+import React from "react";
 
-<<<<<<< HEAD
-import { HashRouter as Router, Route } from 'react-router-dom';
-import Login from './components/Login';
-import SearchDisplay from './components/SearchDisplay';
-import SearchResults from './components/SearchResults';
-import RestaurantMenu from './components/RestaurantMenu';
-import RestaurantProfile from './components/RestaurantProfile';
-=======
 import { HashRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SearchDisplay from "./components/SearchDisplay";
 import SearchResults from "./components/SearchResults";
 import RestaurantMenu from "./components/RestaurantMenu";
+import RestaurantProfile from "./components/RestaurantProfile";
 import UserProfile from "./components/UserProfile";
-import axios from 'axios';
->>>>>>> development
 
 export default class App extends React.Component {
   constructor(props) {
@@ -26,10 +17,10 @@ export default class App extends React.Component {
       console.log(document.cookie);
       const cookieString = document.cookie;
       const id = cookieString.substring(
-        cookieString.indexOf('id=') + 3,
-        cookieString.indexOf(';')
+        cookieString.indexOf("id=") + 3,
+        cookieString.indexOf(";")
       );
-      const token = cookieString.substring(cookieString.indexOf('token=') + 6);
+      const token = cookieString.substring(cookieString.indexOf("token=") + 6);
 
       axios
         .get(
@@ -54,11 +45,8 @@ export default class App extends React.Component {
           <Route path="/login" component={Login} />
           <Route path="/results" component={SearchResults} />
           <Route path="/menu" component={RestaurantMenu} />
-<<<<<<< HEAD
-          <Route path="/profile" component={RestaurantProfile} />
-=======
-          <Route path="/user" component={ UserProfile } />
->>>>>>> development
+          <Route path="/user" component={UserProfile} />
+          <Route path="/customer" component={RestaurantProfile} />
         </div>
       </Router>
     );
