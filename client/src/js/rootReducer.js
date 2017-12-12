33926c1faa6110c430a4a3ff4015a940e030b131
Larry Custodio <custodioly@gmail.com>
2017-12-12 14:36:55 -0800

    Update camelcase for rootreducer

--- client/src/js/rootReducer.js
+++ client/src/js/rootReducer.js
@@ -1,14 +1,14 @@
 import { combineReducers } from 'redux';
 
 import loginReducer from './components/Login/loginReducer';
-import restaurantMenuReducer from './components/RestaurantMenu/RestaurantMenuReducer';
+import restaurantMenuReducer from './components/RestaurantMenu/restaurantMenuReducer';
 import searchDisplayReducer from './components/SearchDisplay/searchDisplayReducer';
 import searchResultsReducer from './components/SearchResults/searchResultsReducer';
 import restaurantProfileReducer from './components/RestaurantProfile/restaurantProfileReducer';
 import userProfileReducer from './components/UserProfile/userProfileReducer';
-import menuItems from './components/MenuItems/MenuItemsReducer';
-import menuOrderSelection from './components/MenuOrderSelection/MenuOrderSelectionReducer';
-import orderCart from './components/OrderCart/OrderCartReducer';
+import menuItems from './components/MenuItems/menuItemsReducer';
+import menuOrderSelection from './components/MenuOrderSelection/menuOrderSelectionReducer';
+import orderCart from './components/OrderCart/orderCartReducer';
 
 const rootReducer = combineReducers({
   login: loginReducer,
